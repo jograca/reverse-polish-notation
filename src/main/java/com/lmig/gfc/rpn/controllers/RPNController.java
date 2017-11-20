c1b7fb0af573c3e84e28eb711373af698303530d
Jon Graca <jon.graca@libertymutual.com>
2017-11-20 11:16:39 -0500

    Updating HTML Form

--- src/main/java/com/lmig/gfc/rpn/controllers/RPNController.java
+++ src/main/java/com/lmig/gfc/rpn/controllers/RPNController.java
@@ -8,28 +8,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RPNController {
-	
+
 	@GetMapping("/")
 	public ModelAndView showCalculator() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("index");
-		
+
 		return mv;
 	}
-	
-	@PostMapping("/calculate")
+
+	@PostMapping("/enter")
 	public ModelAndView calculateResult() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("index");
-		
-		return mv;
-	}
-	
-	@PostMapping("/reset")
-	public ModelAndView resetCalculator() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("index");
-		
+
 		return mv;
 	}
 }
