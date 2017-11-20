7b5b03ad59d4be322e2cc71fcd6fb13e51392537
Jon Graca <jon.graca@libertymutual.com>
2017-11-20 12:16:10 -0500

    Updating Controller

--- src/main/java/com/lmig/gfc/rpn/controllers/RPNController.java
+++ src/main/java/com/lmig/gfc/rpn/controllers/RPNController.java
@@ -1,5 +1,7 @@
 package com.lmig.gfc.rpn.controllers;
 
+import java.util.Stack;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,19 +11,41 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RPNController {
 
+	private Stack<Double> stack;
+	
+	// Constructor
+	public RPNController () {
+		this.stack = new Stack<Double>(); 
+	}
+
 	@GetMapping("/")
 	public ModelAndView showCalculator() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("index");
+		mv.addObject("stack", stack);
 
 		return mv;
 	}
 
 	@PostMapping("/enter")
-	public ModelAndView calculateResult() {
+	public ModelAndView pushNumberOntoStack(double value) {
+		stack.push(value);
+		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("index");
+		mv.setViewName("redirect:/");
 
 		return mv;
 	}
+	
+	@PostMapping("/pop")
+	public ModelAndView resetStack() {
+
+		stack.pop();
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+
+		return mv;
+	}
+	
 }
